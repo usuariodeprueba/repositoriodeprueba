4b340c5c84865b4fe29a6409b6b4a5293acd6f1a
Usuario <kismet@vmubuntu-kismet.(none)>
2012-04-14 21:18:33 +0200

    Primer commit

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+/**
+*         Calcula el valor de un factorial.
+*/
+ 
+public class Factorial {
+ 
+  public int calcularFactorial(int i) {
+    if (i > 0) {
+      return i * calcularFactorial(i - 1);
+    } else {
+      return 1;
+    }
+  }
+}
